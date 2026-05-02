9e1f65578dd757ec3acc19795bc31e15c7b8b2f5
Rin Iwai <me@eyrin.jp>
2026-05-02 22:01:42 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -59,106 +59,103 @@ interface SpotifyNowPlaying {
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-export default {
-  // Access Tokenを使用して再生中の音楽を取得
-  async fetch(
-    request: Request,
-    env: Env,
-    _ctx: ExecutionContext,
-  ): Promise<Response> {
-    if (env.REDIRECT_URL) {
-      const code = new URL(request.url).searchParams.get("code");
-      if (!code) {
-        return Response.redirect(
-          `https://accounts.spotify.com/authorize?${new URLSearchParams({
-            response_type: "code",
-            client_id: env.CLIENT_ID,
-            scope: "user-read-currently-playing",
-            redirect_uri: env.REDIRECT_URL,
-          })}`,
-          302,
-        );
-      }
-
-      const res: { access_token: string; refresh_token: string } = await fetch(
-        "https://accounts.spotify.com/api/token",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: `Basic ${btoa(env.CLIENT_ID + ":" + env.CLIENT_SECRET)}`,
-          },
-          body: new URLSearchParams({
-            grant_type: "authorization_code",
-            code: code,
-            redirect_uri: env.REDIRECT_URL,
-          }),
-        },
-      ).then((res) => {
-        if (res.status !== 200) {
-          throw new Error("Spotify API Error");
-        }
-        return res.json();
-      });
-
-      await env.KV.put("access-token", res.access_token);
-      await env.KV.put("refresh-token", res.refresh_token);
-
-      return Response.json({
-        access_token: res.access_token,
-        refresh_token: res.refresh_token,
-      });
+app.get("/", async (c) => {
+  if (c.env.REDIRECT_URL) {
+    const code = new URL(c.req.url).searchParams.get("code");
+    if (!code) {
+      return Response.redirect(
+        `https://accounts.spotify.com/authorize?${new URLSearchParams({
+          response_type: "code",
+          client_id: c.env.CLIENT_ID,
+          scope: "user-read-currently-playing",
+          redirect_uri: c.env.REDIRECT_URL,
+        })}`,
+        302,
+      );
     }
 
-    const accessToken = await env.KV.get("access-token");
-    if (!accessToken) {
-      throw new Error("access-token is null");
-    }
-
-    const res: SpotifyAPIResponse = await fetch(
-      "https://api.spotify.com/v1/me/player/currently-playing?market=JP",
+    const res: { access_token: string; refresh_token: string } = await fetch(
+      "https://accounts.spotify.com/api/token",
       {
-        method: "GET",
-        headers: { Authorization: `Bearer ${accessToken}` },
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: `Basic ${btoa(c.env.CLIENT_ID + ":" + c.env.CLIENT_SECRET)}`,
+        },
+        body: new URLSearchParams({
+          grant_type: "authorization_code",
+          code: code,
+          redirect_uri: c.env.REDIRECT_URL,
+        }),
       },
     ).then((res) => {
-      if (res.status === 204) {
-        return {};
-      }
       if (res.status !== 200) {
         throw new Error("Spotify API Error");
       }
       return res.json();
     });
 
-    const nowPlaying: SpotifyNowPlaying = {
-      album: {
-        name: res.item?.album?.name,
-        url: res?.item?.album?.external_urls.spotify,
-      },
-      artists:
-        res.item?.artists?.map((artist) => ({
-          name: artist.name,
-          url: artist.external_urls?.spotify,
-        })) ?? [],
-      duration_ms: res.item?.duration_ms,
-      images:
-        res.item?.album?.images.map((image) => ({
-          url: image.url,
-          height: image.height,
-          width: image.width,
-        })) ?? [],
-      is_playing: res.is_playing,
-      name: res.item?.name,
-      progress_ms: res.progress_ms,
-      url: res.item?.external_urls?.spotify,
-    };
-    return Response.json(nowPlaying, {
-      headers: {
-        "Access-Control-Allow-Origin": "https://iwair.in",
-      },
+    await c.env.KV.put("access-token", res.access_token);
+    await c.env.KV.put("refresh-token", res.refresh_token);
+
+    return Response.json({
+      access_token: res.access_token,
+      refresh_token: res.refresh_token,
     });
-  },
+  }
+
+  const accessToken = await c.env.KV.get("access-token");
+  if (!accessToken) {
+    throw new Error("access-token is null");
+  }
+
+  const res: SpotifyAPIResponse = await fetch(
+    "https://api.spotify.com/v1/me/player/currently-playing?market=JP",
+    {
+      method: "GET",
+      headers: { Authorization: `Bearer ${accessToken}` },
+    },
+  ).then((res) => {
+    if (res.status === 204) {
+      return {};
+    }
+    if (res.status !== 200) {
+      throw new Error("Spotify API Error");
+    }
+    return res.json();
+  });
+
+  const nowPlaying: SpotifyNowPlaying = {
+    album: {
+      name: res.item?.album?.name,
+      url: res?.item?.album?.external_urls.spotify,
+    },
+    artists:
+      res.item?.artists?.map((artist) => ({
+        name: artist.name,
+        url: artist.external_urls?.spotify,
+      })) ?? [],
+    duration_ms: res.item?.duration_ms,
+    images:
+      res.item?.album?.images.map((image) => ({
+        url: image.url,
+        height: image.height,
+        width: image.width,
+      })) ?? [],
+    is_playing: res.is_playing,
+    name: res.item?.name,
+    progress_ms: res.progress_ms,
+    url: res.item?.external_urls?.spotify,
+  };
+  return Response.json(nowPlaying, {
+    headers: {
+      "Access-Control-Allow-Origin": "https://iwair.in",
+    },
+  });
+});
+
+export default {
+  fetch: app.fetch,
 
   // 30分ごとにAccess Tokenを更新
   async scheduled(_event: ScheduledEvent, env: Env, _ctx: ExecutionContext) {
